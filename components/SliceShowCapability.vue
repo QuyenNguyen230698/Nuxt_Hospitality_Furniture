f613173c806fdebef35c31fc9c94ba1f95b713f9
Quyen <at128ve@gmail.com>
2024-11-07 13:00:08 +0700

    update page capability

---
+++ components/SliceShowCapability.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="py-10 md:py-14 border-b border-b-orange-500 relative overflow-hidden">
+        <div class="absolute inset-0 h-full bg-slice-2 pointer-events-none z-30"></div>
+            <div class="absolute inset-0 bg-slice" :class="{ 'animate-scale': scaleAnimate }" :style="{ backgroundImage: `url(${currentImage.src})` }"></div>
+            <div class="flex flex-col w-full h-full relative z-40">
+              <div class="flex flex-col justify-center items-center text-white container m-8 mx-auto">
+                <h2 class=" font-black text-center leading-snug tracking-widest uppercase" style="line-height: 3.5;">
+                  <span class="text-5xl">{{ $t('app.sliceShow.tophospitality') }}</span>
+                  <br>
+                  <span class="text-5xl">{{ $t('app.sliceShow.inVietnam') }}</span>
+                </h2>
+              </div>
+              <div class="flex flex-col md:flex-row justify-center items-center gap-4 w-full">
+                <button @click="showVideo=true" class="bg-none btn-hover hover:border-white hover:text-white transition-colors duration-300 border-2 w-2/5 md:w-2/6 lg:w-1/5 h-10 lg:h-12 text-md leading-none border-orange-500 text-orange-500 rounded-md uppercase">{{ $t('app.sliceShow.factorytour') }}</button>
+                <button class="transition-colors duration-300 border-2 w-2/5 md:w-2/6 lg:w-1/5 h-10 lg:h-12 text-md leading-none btn-hover2 rounded-md uppercase">{{ $t('app.sliceShow.capabilityprofile') }}</button>
+              </div>
+            </div>
+            <!-- video show -->
+            <div>
+              <!-- Video Modal -->
+              <div v-if="showVideo" class="fixed inset-0 flex items-center justify-center bg-black" style="z-index: 9999;">
+                <div class="relative w-full h-full max-w-full max-h-full">
+                  <!-- Video Element -->
+                  <video
+                      ref="videoPlayer"
+                  src="https://api.tranduc.com/api/video/TDF_Factory.mp4"
+                  controls
+                  autoplay
+                  class="w-full h-full object-contain"></video>
+          
+                <!-- Close Button -->
+                <button @click="closeVideo" class="absolute top-4 right-4 text-white text-3xl">&times;</button>
+                </div>
+              </div>
+            </div>
+         </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// Danh sách hình ảnh
+const slideImage = [
+  { src: '/image/capability/cap1.jpg' },
+  { src: '/image/capability/cap2.jpg' },
+  { src: '/image/capability/cap3.jpg' },
+  { src: '/image/capability/cap4.jpg' },
+  { src: '/image/capability/cap5.jpg' },
+]
+//#region QuyenNC ( khởi tạo các biến )
+const scaleAnimate = ref(true)
+const currentImage = ref(slideImage[0])
+const showVideo = ref(false);
+let currentIndex = 0
+const offScale = () => {
+  scaleAnimate.value = false
+}
+//#endregion
+  // Function to close video and pause playback
+  function closeVideo() {
+    showVideo.value = false;
+    const videoPlayer = document.querySelector('video');
+    if (videoPlayer) videoPlayer.pause();
+  }
+// Khởi tạo hàm để thay đổi hình ảnh
+function changeImage() {
+  currentIndex = (currentIndex + 1) % slideImage.length
+  currentImage.value = slideImage[currentIndex]
+  scaleAnimate.value = true
+}
+// setInterval để thay đổi hình ảnh và tắt/ bật animation
+onMounted(() => {
+    setInterval(changeImage, 3000)
+    setInterval(offScale, 2995)
+
+})
+</script>
+
+<style scoped>
+.bg-slice {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+/* Tự động scale trong 3 giây */
+.animate-scale {
+  animation: scaleUp 3s ease-in-out forwards; 
+}
+.bg-slice-2 {
+  background-color: #1b1b24;
+  opacity: 0.76;
+}
+/* factorytour button */
+.btn-hover:hover {
+  background-color: #7A7A7A;
+}
+/* capabilityprofile button */
+.btn-hover2 {
+  background-color: #7B7051;
+  border-color: #B9A97A;
+  color: white
+}
+.btn-hover2:hover {
+  background-color: rgba(255,255,255,0);
+  color:#B9A97A;
+  border-color: #B9A97A;
+}
+.fixed {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+@keyframes scaleUp {
+  0% {
+    transform: scale(1); /* Kích thước ban đầu */
+  }
+  100% {
+    transform: scale(1.2); /* Phóng to 1.2 lần */
+  }
+}
+</style>
--- pages/Capabilities/index.vue
+++ pages/Capabilities/index.vue
@@ -1,7 +1,98 @@
 <template>
     <div>
-        <h2>Capabilities</h2>
-        <a href="/">Home</a>
+        <SliceShowCapability/>
+        <div class="flex flex-col w-full h-full">
+            <!-- Wooden Furniture -->
+            <div class="flex flex-col md:flex-row h-full w-full bg-black">
+                <div class="w-full md:w-3/5">
+                    <img src="/public/image/capability/capWooden.jpg" class="w-full h-fit object-cover ">
+                </div>
+                <div class="w-full md:w-2/5 flex flex-col p-10">
+                    <h2 class="text-center text-5xl font-bold p-6 text-white">
+                        {{ $t("app.capability.wooden") }}
+                    </h2>
+                    <div
+                        class="w-40 md:w-24 lg:w-20 bg-orange-500 text-center mx-auto mb-5"
+                        style="height: 1px"
+                    ></div>
+                    <p class="text-base color-section text-justify">
+                        {{ $t("app.capability.23years") }}
+                    </p>
+                </div>
+            </div>
+            <!-- Metal Furniture -->
+            <div class="flex flex-col md:flex-row h-full w-full bg-metal">
+                <div class="w-full md:w-2/5 flex flex-col p-10">
+                    <h2 class="text-center text-5xl font-bold p-6 text-white">
+                        {{ $t("app.capability.metal") }}
+                    </h2>
+                    <div
+                        class="w-40 md:w-24 lg:w-20 bg-orange-500 text-center mx-auto mb-5"
+                        style="height: 1px"
+                    ></div>
+                    <p class="text-base color-section text-justify">
+                        {{ $t("app.capability.tranDucMetal") }}
+                    </p>
+                </div>
+                <div class="w-full md:w-3/5 overflow-hidden" style="height: 500px;">
+                    <img src="/public/image/capability/cap5.jpg" class="w-full h-full object-cover ">
+                </div>
+            </div>
+            <!-- Ulpholstery -->
+            <div class="flex flex-col md:flex-row h-full w-full bg-black">
+                <div class="w-full md:w-3/5 overflow-hidden" style="height: 500px;">
+                    <img src="/public/image/capability/capUlpho.jpg" class="w-full h-full object-cover ">
+                </div>
+                <div class="w-full md:w-2/5 flex flex-col p-10">
+                    <h2 class="text-center text-5xl font-bold p-6 text-white">
+                        {{ $t("app.capability.upholstery") }}
+                    </h2>
+                    <div
+                        class="w-40 md:w-24 lg:w-20 bg-orange-500 text-center mx-auto mb-5"
+                        style="height: 1px"
+                    ></div>
+                    <p class="text-base color-section text-justify">
+                        {{ $t("app.capability.tranDucUpholstery") }}
+                    </p>
+                </div>
+            </div>
+            <!-- Door -->
+            <div class="flex flex-col md:flex-row h-full w-full bg-metal">
+                <div class="w-full md:w-2/5 flex flex-col p-10">
+                    <h2 class="text-center text-5xl font-bold p-6 text-white">
+                        {{ $t("app.capability.door") }}
+                    </h2>
+                    <div
+                        class="w-40 md:w-24 lg:w-20 bg-orange-500 text-center mx-auto mb-5"
+                        style="height: 1px"
+                    ></div>
+                    <p class="text-base color-section text-justify">
+                        {{ $t("app.capability.tranDucDoor") }}
+                    </p>
+                </div>
+                <div class="w-full md:w-3/5 overflow-hidden" style="height: 500px;">
+                    <img src="/public/image/capability/capDoor.jpg" class="w-full h-full object-cover ">
+                </div>
+            </div>
+            <!-- Finishing -->
+            <div class="flex flex-col md:flex-row h-full w-full bg-black">
+                <div class="w-full md:w-3/5 overflow-hidden" style="height: 500px;">
+                    <img src="/public/image/capability/capFinishing.jpg" class="w-full h-full object-cover ">
+                </div>
+                <div class="w-full md:w-2/5 flex flex-col p-10">
+                    <h2 class="text-center text-5xl font-bold p-6 text-white">
+                        {{ $t("app.capability.finishing") }}
+                    </h2>
+                    <div
+                        class="w-40 md:w-24 lg:w-20 bg-orange-500 text-center mx-auto mb-5"
+                        style="height: 1px"
+                    ></div>
+                    <p class="text-base color-section text-justify">
+                        {{ $t("app.capability.tranDucFinishing") }}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,5 +101,13 @@
 </script>
 
 <style scoped>
-
+.color-text {
+  color: #ababab;
+}
+.color-section {
+  color: #bfbfbf;
+}
+.bg-metal {
+    background-color: #54595E;
+}
 </style>
