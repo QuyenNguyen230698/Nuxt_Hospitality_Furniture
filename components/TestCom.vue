b3a0833bdef7abb58f7096f2aa739bb044a8906a
Quyen <at128ve@gmail.com>
2024-11-26 17:37:08 +0700

    update new testCom

--- components/TestCom.vue
+++ components/TestCom.vue
@@ -1,5 +1,9 @@
 <template>
-  <Flipbook class="flipbook" :pages="['https://upload.wikimedia.org/wikipedia/commons/9/95/Latin_G.png','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGJKjsKDzUnQUP7sPcIiKWtLbWy9WbeUg0Yw&s','https://www.planerium.com/wp-content/uploads/2020/11/alphabet-coloring-page-english-letter-G-capital-planerium.jpg']" />
+  <div class="flex items-center justify-center w-full h-screen">
+    <h2 class="text-sm md:text-lg lg:text-2xl">No product information yet!</h2>
+    <NuxtLink to="/" class="text-orange-500 text-sm md:text-lg lg:text-2xl font-bold">Go home</NuxtLink>
+  </div>
+  <!-- <Flipbook class="flipbook" :pages="['https://upload.wikimedia.org/wikipedia/commons/9/95/Latin_G.png','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGJKjsKDzUnQUP7sPcIiKWtLbWy9WbeUg0Yw&s','https://www.planerium.com/wp-content/uploads/2020/11/alphabet-coloring-page-english-letter-G-capital-planerium.jpg']" /> -->
 </template>
 <script setup>
 import Flipbook from 'flipbook-vue'
